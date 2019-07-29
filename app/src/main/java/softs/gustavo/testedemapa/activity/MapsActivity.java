bac3d9140d0b0e7c55997e238cf2a19c21445dec
guscode <gustavo.softs@gmail.com>
2019-07-29 18:34:27 -0300

    esta localizando usuario

--- app/src/main/java/softs/gustavo/testedemapa/activity/MapsActivity.java
+++ app/src/main/java/softs/gustavo/testedemapa/activity/MapsActivity.java
@@ -1,32 +1,45 @@
 package softs.gustavo.testedemapa.activity;
 
-import android.support.v4.app.FragmentActivity;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.auth.FirebaseAuth;
 
 import softs.gustavo.testedemapa.R;
+import softs.gustavo.testedemapa.config.ConfiguracaoFireBase;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private FirebaseAuth autenticacao;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        //Configurações iniciais
+        autenticacao = ConfiguracaoFireBase.getFirebaseAutenticacao();
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
-
-
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -39,10 +52,49 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
+        recuperarLocalizacaoUsuario();
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+    }
+    private void recuperarLocalizacaoUsuario() {
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Double latitude = location.getLatitude();
+                Double longitude = location.getLongitude();
+                LatLng localUsuario = new LatLng(latitude, longitude);
+                mMap.clear();
+                mMap.addMarker(
+                        new MarkerOptions()
+                                .position(localUsuario)
+                                .title("Meu Local")
+                                .icon(BitmapDescriptorFactory.fromResource(R.drawable.usuario))
+                );
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(localUsuario, 20));
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER, 10000, 10, locationListener
+            );
+        }
+
+
     }
 }
