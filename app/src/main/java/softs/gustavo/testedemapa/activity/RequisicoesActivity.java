c1d8248e0f75fdeac2e0cddb4eadf349ca4ae4c3
guscodes <gustavo.softs@gmail.com>
2019-08-21 17:21:28 -0300

    recycle view add -[] funcional???

--- app/src/main/java/softs/gustavo/testedemapa/activity/RequisicoesActivity.java
+++ app/src/main/java/softs/gustavo/testedemapa/activity/RequisicoesActivity.java
@@ -2,14 +2,26 @@ package softs.gustavo.testedemapa.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import softs.gustavo.testedemapa.R;
 
 public class RequisicoesActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_requisicoes);
+        recyclerView = findViewById(R.id.RecycleVw);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.addOnItemTouchListener(R.id.sinal_motorista);
+
+
     }
 }
